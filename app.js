d6c455660c3d3913827d65b97cd0fd6c96c7d4b7
Cameron Kenny <55995394+cameronkenny101@users.noreply.github.com>
2021-01-01 21:50:32 +0000

    Data sent from bug report form to nodeExpress

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
 var routes = require('./routes/index');
 var bugs = require('./routes/bugs');
 var devs = require('./routes/devs');
 
-var app = express();
+var element = [];
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/public', express.static('public'));
 
+app.get('/bugs', function (req, res) {
+    res.render('bugs', {element, element});
+});
+
 app.use("/", routes);
-app.use("/bugs", bugs);
 app.use("/devs", devs);
 
-app.get("/", function(req, res) {
-    res.render("index.ejs");
-});
+app.post("/newBug", function(req, res) {
+    console.log("Request Sent!");
+    var name = req.body.title;
+    var description = req.body.description;
+    var priority = req.body.priorityInput;
+    element.push(name);
+    element.push(description);
+    element.push(priority);
+    console.log(element);
+    res.redirect("/bugs");
+})
 
 app.listen(3000, function() {
     console.log("Server started from port 3000");
