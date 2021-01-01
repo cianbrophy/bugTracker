2a61f4da0e3823ae9c27b769e467650524ac472e
Cameron Kenny <55995394+cameronkenny101@users.noreply.github.com>
2021-01-01 18:04:14 +0000

    Added Node.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var routes = require('./routes/index');
+var bugs = require('./routes/bugs');
+
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use('/public', express.static('public'));
+
+app.use("/", routes);
+app.use("/bugs", bugs);
+
+app.get("/", function(req, res) {
+    res.render("index.ejs");
+});
+
+app.listen(3000, function() {
+    console.log("Server started from port 3000");
+});
