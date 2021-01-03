e0730731deb521cdb434b5f91075f5906df20707
cianbrophy <cianbrophy7@gmail.com>
2021-01-03 16:56:53 +0000

    devs add and remove functions added (WIP)

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var mongodb = require('mongodb');
 
-mongoose.connect("mongodb://localhost/bug", {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+mongoose.connect("mongodb://localhost/dev", {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
     if (err)
      console.error(err);
   else
      console.log("Connected to the mongodb"); 
 });
+
 
 var routes = require('./routes/index');
 var devs = require('./routes/devs');
@@ -20,7 +21,14 @@ var bugSchema = new mongoose.Schema({
     priority: String
 });
 
+var devSchema = new mongoose.Schema({
+    firstname: String,
+    secondname: String
+});
+
 var Bug = mongoose.model("Bug", bugSchema);
+
+var Dev = mongoose.model("Dev", devSchema);
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -33,6 +41,16 @@ app.get('/bugs', function (req, res) {
             console.log("hello");
         else {
             res.render('bugs', {element, element});
+        }
+    })
+});
+
+app.get('/devs', function (req, res) {
+    Dev.find({}, function(err, element) {
+        if(err)
+            console.log("hello");
+        else {
+            res.render('devs', {element, element});
         }
     })
 });
@@ -71,6 +89,38 @@ app.post("/newBug", function(req, res) {
 
     res.redirect("/bugs");
 });
+
+app.post("/newDev", function(req, res) {
+    console.log("Dev Request Sent!");
+    var newDev = new Dev({
+        firstname: req.body.firstname,
+        secondname: req.body.secondname
+    });
+
+    Bug.create(newDev, function(err, Dev) {
+        if(err)
+            console.log(err);
+        else
+            console.log("Inserted Bug " + newDev);
+    })
+
+    res.redirect("/devs");
+});
+
+app.post("/deleteDev", function(req, res) {
+    console.log("Delete Sent!");
+
+    Dev.deleteOne({_id: new mongodb.ObjectID(req.body.id)}, function(err, results) {
+        if (err){
+            console.log("failed");
+            throw err;
+          }
+          console.log("success");
+       });
+
+    res.redirect("/devs");
+});
+
 
 app.listen(3000, function() {
     console.log("Server started from port 3000");
